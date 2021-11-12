7ed3f96b0f246f371a313e998e16ff826dc00af3
vishal <vrana14693@gmail.com>
2021-11-12 13:41:09 +0530

    Intro component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-function App() {
-  return <div className="App">Hello</div>;
-}
+import React from "react";
+import Intro from "./components/intro/Intro";
+const App = () => {
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
+};
 
 export default App;
