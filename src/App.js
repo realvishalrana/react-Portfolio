bd35987550f0e7b5a519abd428225a6f31abfaa6
vishal <vrana14693@gmail.com>
2021-11-12 17:02:18 +0530

    product and productList components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import About from "./components/about/About";
 import Intro from "./components/intro/Intro";
+import ProductList from "./components/productList/ProductList";
 const App = () => {
   return (
     <div>
       <Intro />
       <About />
+
+      <ProductList />
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://source.unsplash.com/user/erondu/271x242",
+    link: "https://github.com/realvishalrana",
+  },
+  {
+    id: 2,
+    img: "https://source.unsplash.com/user/erondu/271x242",
+    link: "https://github.com/realvishalrana",
+  },
+  {
+    id: 3,
+    img: "https://source.unsplash.com/user/erondu/271x242 ",
+    link: "https://github.com/realvishalrana",
+  },
+  {
+    id: 4,
+    img: "https://source.unsplash.com/user/erondu/271x242 ",
+    link: "https://github.com/realvishalrana",
+  },
+  {
+    id: 5,
+    img: "https://source.unsplash.com/user/erondu/271x242 ",
+    link: "https://github.com/realvishalrana",
+  },
+  {
+    id: 6,
+    img: "https://source.unsplash.com/user/erondu/271x242 ",
+    link: "https://github.com/realvishalrana",
+  },
+];
