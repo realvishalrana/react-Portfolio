cd2767de202de2b90819b1fba9b5184321f8d56a
vishal <vrana14693@gmail.com>
2021-11-13 16:48:08 +0530

    imgs and favicon added

--- src/data.js
+++ src/data.js
@@ -1,5 +1,10 @@
 import paint from "./img/paint-box.png";
 import horror from "./img/horror.png";
+import tolv from "./img/Tolv-Coding.png";
+import zoo from "./img/zoo-coding.png";
+import canal from "./img/canalstreet.png";
+import schwartz from "./img/schwartz.png";
+
 export const products = [
   {
     id: 1,
@@ -13,22 +18,22 @@ export const products = [
   },
   {
     id: 3,
-    img: "https://weandthecolor.com/wp-content/uploads/2016/04/New-Master-oil-on-canvas-painting-in-the-size-of-60x80cm.jpg",
-    link: "https://github.com/realvishalrana",
+    img: tolv,
+    link: "https://realvishalrana.github.io/scss-projects/scss-masterclass-Tolv-Coding/",
   },
   {
     id: 4,
-    img: "https://quotes-image.com/assets/upload/quotes/quotes_12101/quotes143_1595082293.jpg",
-    link: "https://github.com/realvishalrana",
+    img: zoo,
+    link: "https://realvishalrana.github.io/scss-projects/scss-masterclass-Zoo-Coding/",
   },
   {
     id: 5,
-    img: "https://i.pinimg.com/474x/5e/6d/66/5e6d66f4e87c251afee6c9c580f6c00c.jpg",
-    link: "https://github.com/realvishalrana",
+    img: canal,
+    link: "https://realvishalrana.github.io/scss-projects/scss-masterclass-Canal-Street/",
   },
   {
     id: 6,
-    img: "https://i0.wp.com/www.fonewalls.com/wp-content/uploads/1242x2688-Background-HD-Wallpaper-025-300x649.jpg?resize=816%2C9999&ssl=1 ",
-    link: "https://github.com/realvishalrana",
+    img: schwartz,
+    link: "https://realvishalrana.github.io/scss-projects/scss-masterclass-Schwartz%20Coding/",
   },
 ];
